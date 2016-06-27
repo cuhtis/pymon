afa612bb7b8a1949ca2ed560998bff9b5060c9f2
Curtis <curtis99877@gmail.com>
2016-06-26 20:28:57 -0400

    Move main code to run.py

--- pymon.py
+++ pymon.py
@@ -1,61 +1,7 @@
 #!/usr/bin/env python
 
-import sys
-from watchdog.observers import Observer
-from lib.settings import parse_settings
-from lib.watch import PymonEventHandler
-from lib.listener import PymonListener
-from lib.transport import transport
-from lib.debug import *
-
-
-def run():
-    # Create and read all the settings
-    settings = parse_settings()
-    debug(settings)
-
-    colour_print(BLUE_COLOUR, "[pymon] Pymon %s" % settings["version"])
-    colour_print(BLUE_COLOUR, "[pymon] Watching %s" % settings["path"])
-
-    # Create an event handler for file system changes
-    event_handler = PymonEventHandler(
-            settings["regexes"], 
-            settings["ignores"], 
-            True, False)
-
-    # Create a file system observer
-    observer = Observer()
-    observer.schedule(
-            event_handler, 
-            settings["path"], 
-            recursive=True)
-    observer.start()
-
-    # Create and register a listener for the user's application
-    listener = PymonListener(
-            settings["prog"], 
-            settings["app_args"])
-    transport.add_listener(listener)
-
-    try:
-        # Loop for user input
-        while True:
-            user_input = raw_input("")
-
-            if user_input == "rs" or user_input == "restart":
-                transport.emit("restart")
-            elif user_input == "stop":
-                raise Exception
-
-    except (KeyboardInterrupt, Exception) as e:
-        # Terminate all applications and exit
-        transport.emit("stop")
-        observer.stop()
-        observer.join()
+from lib.run import run
 
 
 if __name__ == "__main__":
     run()
-else:
-    warn("Non-CLI usage has not been supported yet")
-    sys.exit(0)
