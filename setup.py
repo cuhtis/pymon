4a50226f6a1b174e6cd2076c7475ecac201e45a8
Curtis <curtis99877@gmail.com>
2016-07-21 03:36:13 +0800

    pip files

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+
+setup(
+    name = 'pymon',
+    packages = ['pymon'],
+    version = '1.1',
+    description = 'Application monitoring in Python',
+    author = 'Curtis Li',
+    author_email = 'curtis.li@nyu.edu',
+    url = 'https://github.com/curtis99877/pymon',
+    download_url = 'https://github.com/curtis99877/pymon/tarball/v1.1',
+    keywords = ['web', 'application', 'server', 'dev', 'tools', 'monitor', 'restart'],
+    classifiers = [],
+    )
